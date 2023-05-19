f17e641007ad842a074a8ff73831d0f3f613ee27
jialin-yu <zhwysh@outlook.com>
2023-05-19 11:22:26 +0100

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,11 +1,11 @@
-# wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-# wget http://qim.fs.quoracdn.net/quora_duplicate_questions.tsv
-# unzip annotations_trainval2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget http://qim.fs.quoracdn.net/quora_duplicate_questions.tsv
+unzip annotations_trainval2017.zip
 
-# mkdir ../.data
-# mv quora_duplicate_questions.tsv ../.data/quora_duplicate_questions.tsv
-# mv annotations/ ../.data/annotations/
-# rm annotations_trainval2017.zip
+mkdir ../.data
+mv quora_duplicate_questions.tsv ../.data/quora_duplicate_questions.tsv
+mv annotations/ ../.data/annotations/
+rm annotations_trainval2017.zip
 
 wget http://cs.jhu.edu/~vandurme/data/parabank-1.0-5m-diverse.zip
 unzip parabank-1.0-5m-diverse.zip
